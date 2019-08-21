8baeb4f26d144df77875895060a891a4312ab760
Paul Rosenthal <42185344+PaulRosenthal@users.noreply.github.com>
2019-08-21 10:21:02 -0500

    Add URL Variable to Test with CURL

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -l
 
-sh -c "echo Initiating Test Type:  $TYPE_OF_TEST"
-sh -c "echo Results for $TYPE_OF_TEST to www.google.com:"
-CURL_RESPONSE = `curl www.google.com`
-sh -c "echo $CURL_RESPONSE"
+echo "Initiating Test Type:  $TYPE_OF_TEST"
+echo "Results for $TYPE_OF_TEST to the URL $URL:"
+CURL_RESPONSE = $(curl -v $URL)
+echo $CURL_RESPONSE
+
