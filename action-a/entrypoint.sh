f1fc6537462c78dcac2df736ca0160c296a5c01b
Paul Rosenthal <42185344+PaulRosenthal@users.noreply.github.com>
2019-08-21 10:26:32 -0500

    Resolve echo Syntax Error (2)

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -2,6 +2,6 @@
 
 echo "Initiating Test Type:  $TYPE_OF_TEST"
 echo "Results for $TYPE_OF_TEST to the URL $URL:"
-CURL_RESPONSE = $(curl -v $URL)
+CURL_RESPONSE=$(curl -v $URL)
 echo "$CURL_RESPONSE"
 
